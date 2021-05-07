b7257d53007f45ff2d51afdb0d8181fd8929608f
AlfredoCM <joy.porta@gmail.com>
2021-05-07 11:07:53 -0500

    Reto 1 listo

---
+++ RETO 1 MC/tarea.js
@@ -0,0 +1,26 @@
+
+
+
+var pesolui = 92
+var estalui =1.95
+var pesomar = 78
+var estamar = 1.69
+
+var imcl;
+var imcm;
+console.log("MUESTRA 1")
+imcl = (pesolui/(estalui**2))
+imcm = (pesomar/(estamar**2))
+console.log(imcm>imcl?"MarioMayorIMC":"MarioMenorIMC");
+console.log('')
+
+console.log("MUESTRA 2");
+
+pesolui = 85
+estalui =1.76
+pesomar = 95
+estamar = 1.88
+
+imcl = (pesolui/(estamar**2))
+imcm = (pesomar/(estamar**2))
+console.log(imcm>imcl?"MarioMayorIMC":"MarioMenorIMC");
